bba49897e0df44091e5d2cbd4fd69798999434e6
Karolina Nowak <nowak.karolina90@gmail.com>
2019-06-12 16:03:52 +0200

    Add changes to index.html, script.js, functions.js and change styles add class Cart in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
     const select = {
         templateOf: {
             menuProduct: '#template-menu-product',
+            cartProduct: '#template-cart-product', // CODE ADDED
         },
         containerOf: {
             menu: '#product-list',
@@ -26,11 +27,31 @@
         },
         widgets: {
             amount: {
-                input: 'input[name="amount"]',
+                input: 'input.amount', // CODE CHANGED
                 linkDecrease: 'a[href="#less"]',
                 linkIncrease: 'a[href="#more"]',
             },
         },
+        // CODE ADDED START
+        cart: {
+            productList: '.cart__order-summary',
+            toggleTrigger: '.cart__summary',
+            totalNumber: `.cart__total-number`,
+            totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+            subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+            deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+            form: '.cart__order',
+            formSubmit: '.cart__order [type="submit"]',
+            phone: '[name="phone"]',
+            address: '[name="address"]',
+        },
+        cartProduct: {
+            amountWidget: '.widget-amount',
+            price: '.cart__product-price',
+            edit: '[href="#edit"]',
+            remove: '[href="#remove"]',
+        },
+        // CODE ADDED END
     };
 
     const classNames = {
@@ -38,6 +59,11 @@
             wrapperActive: 'active',
             imageVisible: 'active',
         },
+        // CODE ADDED START
+        cart: {
+            wrapperActive: 'active',
+        },
+        // CODE ADDED END
     };
 
     const settings = {
@@ -45,11 +71,19 @@
             defaultValue: 1,
             defaultMin: 1,
             defaultMax: 9,
-        }
+        }, // CODE CHANGED
+        // CODE ADDED START
+        cart: {
+            defaultDeliveryFee: 20,
+        },
+        // CODE ADDED END
     };
 
     const templates = {
         menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+        // CODE ADDED START
+        cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+        // CODE ADDED END
     };
 
     class Product {
@@ -63,7 +97,7 @@
             thisProduct.initOrderForm();
             thisProduct.initAmountWidget();
             thisProduct.processOrder();
-            console.log('new Product: ', thisProduct);
+            //console.log('new Product: ', thisProduct);
         }
         renderInMenu() {
             const thisProduct = this;
@@ -203,8 +237,8 @@
             self.value = settings.amountWidget.defaultValue;
             self.setValue(self.input.value);
             self.initActions();
-            console.log('AmountWidget', self);
-            console.log('constructor arguments: ', element);
+            //console.log('AmountWidget', self);
+            //console.log('constructor arguments: ', element);
         }
         getElements(element) {
             const self = this;
@@ -243,6 +277,29 @@
             self.element.dispatchEvent(event);
         }
     }
+    class Cart {
+        constructor(element) {
+            const self = this;
+            self.products = [];
+            self.getElements(element);
+            self.initActions();
+            console.log('new Cart: ', self);
+        }
+        getElements(element) {
+            const self = this;
+            self.dom = {};
+            self.dom.wrapper = element;
+            self.dom.toggleTrigger = self.dom.wrapper.querySelector(select.cart.toggleTrigger);
+            //console.log('Cart toggleTrigger: ', self.dom.toggleTrigger);
+        }
+        initActions() {
+            const self = this;
+            self.dom.toggleTrigger.addEventListener('click', function(event) {
+                event.preventDefault();
+                self.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+            });
+        }
+    }
     const app = {
         initMenu: function() {
             const thisApp = this;
@@ -256,15 +313,21 @@
             const thisApp = this;
             thisApp.data = dataSource;
         },
+        initCart: function() {
+            const self = this;
+            const cartElem = document.querySelector(select.containerOf.cart);
+            self.cart = new Cart(cartElem);
+        },
         init: function() {
             const thisApp = this;
-            console.log('*** App starting ***');
-            console.log('thisApp:', thisApp);
-            console.log('classNames:', classNames);
-            console.log('settings:', settings);
-            console.log('templates:', templates);
+            //console.log('*** App starting ***');
+            //console.log('thisApp:', thisApp);
+            //console.log('classNames:', classNames);
+            //console.log('settings:', settings);
+            //console.log('templates:', templates);
             thisApp.initData();
             thisApp.initMenu();
+            thisApp.initCart();
         },
     };
 
